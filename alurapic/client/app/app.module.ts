8508c7c6c6ed49fc023f03e349a502661b2cd0c4
caio.cvac <caio.cvac@dpf.gov.br>
2018-01-16 09:52:22 -0200

    Aula 03

--- alurapic/client/app/app.module.ts
+++ alurapic/client/app/app.module.ts
@@ -1,10 +1,12 @@
+import 'rxjs/add/operator/map';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {FotoModule} from './foto/foto.module';
+import {HttpModule} from '@angular/http';
 
 @NgModule({
-    imports: [BrowserModule, FotoModule],
+    imports: [BrowserModule, FotoModule, HttpModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
